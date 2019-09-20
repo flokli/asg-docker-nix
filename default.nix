a3afa62e6982b33117237ded5c43e0c53f431c3b
Arian van Putten <arian.vanputten@gmail.com>
2019-09-20 15:24:26 +0200

    nginxContainer: setup nobody/nogroup, fix nginx config

--- default.nix
+++ default.nix
@@ -1,28 +1,42 @@
 rec {
   pkgs = import ./nixpkgs {};
 
-  nixos-icon = "${pkgs.nixos-icons}/share/icons/hicolor/512x512/apps/nix-snowflake.png";
+  userSetup = pkgs.runCommand "user-setup" { } ''
+    mkdir -p $out/etc/
+    echo "root:x:0:0:root user:/root:${pkgs.bash}/bin/bash" > $out/etc/passwd
+    echo "nobody:x:65534:65534:nobody:/var/empty:${pkgs.bash}/bin/bash" >> $out/etc/passwd
+    echo "root:x:0:" > $out/etc/group
+    echo "nogroup:x:65534:" >> $out/etc/group
+  '';
 
   indexHtml =
     pkgs.writeTextDir "index.html" ''
       <!doctype htm>
       <h1>Hello from Nix</h1>
-      <img src="${nixos-icon}" alt="NixOS" />
+      <img src="/img.png" alt="NixOS" />
       <h2>A Minimal docker image</h2>
     '';
 
   nginxConfig = pkgs.writeText "nginx.conf" ''
     daemon off;
+
+    events { worker_connections 1024; }
     http {
-      server 80;
-      location / {
-        root ${indexHtml}
-      };
+      server {
+        listen 80;
+        location /img.png {
+          alias ${pkgs.nixos-icons}/share/icons/hicolor/256x256/apps/nix-snowflake.png;
+        }
+        location / {
+          root ${indexHtml};
+        }
+      }
     }
   '';
 
   nginxContainer = pkgs.dockerTools.buildLayeredImage {
     name = "nginx-container";
+    contents = [ userSetup ];
     config = {
       Cmd = [ "${pkgs.nginx}/bin/nginx" "-c" "${nginxConfig}" ];
       ExposedPorts."80" = {};
